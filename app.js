5bfca57b6b8c06d7cc819d0e643cc08b47877dd4
TedNistor <ted_nistor@yahoo.com>
2022-12-16 14:12:18 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+// app.use(express.static("public"));
+const path = require("path");
+const port = process.env.PORT || 3000;
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+mongoose.connect("mongodb://localhost:27017/todo-list");
+const options = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+};
+
+const listSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  date: {
+    type: Date,
+    default: Date.now(),
+    get: (date) => date.toLocaleDateString("en-us", options),
+  },
+});
+
+const List = mongoose.model("Item", listSchema);
+
+app.get("/", (req, res) => {
+  List.find({}, (err, foundItems) => {
+    if (err) console.log(err);
+    res.render("index", {
+      newListItems: foundItems,
+      date: new Date().toLocaleDateString("en-us", options),
+    });
+  }).sort({ date: -1 });
+});
+
+app.post("/", (req, res) => {
+  const itemName = req.body.newItem;
+  List.create({ name: itemName, date: new Date() }, (err, item) =>
+    console.log(err || `Added item: ${item.name} to the list`)
+  );
+  res.redirect("/");
+});
+
+app.post("/delete-item", (req, res) => {
+  const deletedItem = req.body.deleteItem;
+  List.deleteOne({ _id: deletedItem }, (err) =>
+    console.log(err || `Removed item ${deletedItem}`)
+  );
+  res.redirect("/");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
